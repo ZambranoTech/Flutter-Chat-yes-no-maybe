b136f821319c71267d478ae48cce001a449bc57b
Javier García <a2027garciaj@gmail.com>
2023-09-27 22:26:31 +0200

    creada interfaz y input para enviar mensaje, con ciertar funciones.

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://i.pinimg.com/474x/66/5e/18/665e18315fa8224bc2e36e73f0d0ec5d.jpg'),
+          ),
+        ),
+        title: const Text('Mi Amor ❤️'),
+        // centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ( index % 2 == 0) 
+                ? const HerMessageBubble()
+                : const MyMessageBubble();
+              },
+            )),
+            /// CAJA DE TEXTO
+            MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
